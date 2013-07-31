2b60f481c33d8ff48f61b0e2d0c4310e4a7e1106
Guy van den Berg <guy.vandenberg@microsmart.co.za>
2013-07-31 06:56:44 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,74 +1,70 @@
 
 (require 'package)
 
+;; ------------------------------------------------------------------------------
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
+;; ------------------------------------------------------------------------------
 ;; set the theme
 (load-theme 'zenburn t)
-;(load-theme 'solarized-dark t)
 (autoload 'color-theme-approximate-on "color-theme-approximate")
 (color-theme-approximate-on)
 
-;; Allow shift arrow to focus window instead of C-x ot
-;; (windmove-default-keybindings)
-
-;disable backup
+;; ------------------------------------------------------------------------------
+;; disable backup
 (setq backup-inhibited t)
 
+;; ------------------------------------------------------------------------------
+;; full screen mode
+(fullscreen-mode)
+
+;; ------------------------------------------------------------------------------
 ;; add ruby load path
 (add-to-list 'load-path "~/.emacs.d/ruby/")
 
-;; require auto-complete
-;;(add-to-list 'load-path "~/.emacs.d/")
-;;(require 'init-auto-complete)
+;; ------------------------------------------------------------------------------
+;; enable ruby tools mode
+(ruby-tools-mode)
+
+;; ------------------------------------------------------------------------------
+;; start rsense
+(setq rsense-home "/opt/rsense-0.3")
+(add-to-list 'load-path (concat rsense-home "/etc"))
+;; (require 'rsense)
 
 
-; get pry running
+;; ------------------------------------------------------------------------------
+;; get pry running
 (add-to-list 'load-path "~/.emacs.d/ruby/emacs-pry")
 (require 'pry)
-;; optional suggestions
 (global-set-key [S-f9] 'pry-intercept)
 (global-set-key [f9] 'pry-intercept-rerun)
 
-; manage rvm from inside emacs
+;; ------------------------------------------------------------------------------
+;; manage rvm from inside emacs
 (require 'rvm)
 (rvm-use-default) 
 
-; enable rspec mirror mode
-; (require 'rspec-mode)
 
+;; ------------------------------------------------------------------------------
 ;; Add C-# to add method header
 (global-set-key (kbd "C-#")
                 (concat 
-                (make-string 2 ?#)
-                (make-string 1 ?\n)
-                (make-string 1 ?#)
-                (make-string 1 ?\n)
-                (make-string 2 ?#)))
+                 (make-string 2 ?#)
+                 (make-string 1 ?\n)
+                 (make-string 1 ?#)
+                 (make-string 1 ?\n)
+                 (make-string 2 ?#)))
 
 
-; enable robe
-;; (add-hook 'ruby-mode-hook 'robe-mode)
 
-
-;; Rsense
-;; (setq rsense-home "/opt/rsense-0.3")
-;; (add-to-list 'load-path (concat rsense-home "/etc"))
-;; (require 'rsense)
-
-;; Rsense + Autocomplete
-;;(add-hook 'ruby-mode-hook
-;;          (lambda ()
-;;            (add-to-list 'ac-sources 'ac-source-rsense-method)
-;;            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
-
-
+;; ------------------------------------------------------------------------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
